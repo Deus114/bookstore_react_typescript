03db8ae1759ee0030b0a84d23262ba483ffb8e94
DESKTOP-24GU9HH\ASUS <lekhaduy0917880045@gmail.com>
2025-03-10 02:38:23 +0700

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,44 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './layout';
+import BookPage from './pages/client/book';
+import AboutPage from './pages/client/about';
+import LoginPage from './pages/client/auth/login';
+import RegisterPage from './pages/client/auth/register';
+import './styles/global.scss'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/book",
+        element: <BookPage />
+      },
+      {
+        path: "/about",
+        element: <AboutPage />
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <LoginPage />
+  },
+  {
+    path: "/register",
+    element: <RegisterPage />
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    {/* <App /> */}
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
