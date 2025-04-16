4c876fb436c389d0550bfca76517476bcb91f305
DESKTOP-24GU9HH\ASUS <lekhaduy0917880045@gmail.com>
2025-04-17 00:44:57 +0700

    update register api

--- src/services/api.ts
+++ src/services/api.ts
@@ -6,7 +6,7 @@ export const loginApi = (username: string, password: string) => {
 };
 
 export const registerApi = (fullName: string, email: string, password: string, phone: string) => {
-    const backendUrl = "/api/v1/user/register";
+    const backendUrl = "/api/v1/auth/register";
     return axios.post<IBackendRes<IRegister>>(backendUrl, { fullName, email, password, phone });
 };
 
